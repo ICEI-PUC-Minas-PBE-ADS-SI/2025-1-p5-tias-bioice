4d2ff47fc8bf3eb4b916a5a63497aa7c55071594
Saul Netto <saul.emnetto@gmail.com>
2025-06-29 11:28:48 -0300

    feat: Listagem de usuários paginada

---
+++ src/controller/usuario/usuario.controller.ts
@@ -0,0 +1,146 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put, Query,
+  Res,
+} from '@nestjs/common';
+import { CriarUsuarioDto } from '../../model/usuario/dto/criar-usuario.dto';
+import { Usuario } from '../../model/usuario/usuario.entity';
+import { UsuarioService } from '../../service/usuario/usuario.service';
+import { Response } from 'express';
+import {IdDto} from "../../shared/dto/id.dto";
+import { EditarUsuarioDto } from '../../model/usuario/dto/editar-usuario.dto';
+import { MensagensUsuario } from '../../model/usuario/utils/mensagens-usuario';
+import { DataSource } from 'typeorm';
+import {AccessToken} from "../../model/auth/interfaces/access-token.interface";
+import {LoginDTO} from "../../model/usuario/dto/login.dto";
+import {PaginacaoDto} from "../../shared/dto/paginacao.dto";
+import {MensagensInsumos} from "../../model/insumo/utils/mensgens-insumos";
+import {RespostaPaginada} from "../../shared/interfaces/resposta-paginada.interface";
+
+@Controller('usuario')
+export class UsuarioController {
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private dataSource: DataSource
+  ) { }
+
+  @Post()
+  async criarUsuario(
+    @Body() criarUsuarioDto: CriarUsuarioDto,
+    @Res() response: Response,
+  ): Promise<Response> {
+    try {
+      const usuarioCriado: Usuario =
+        await this.usuarioService.criarUsuario(criarUsuarioDto);
+      return response.status(HttpStatus.CREATED).send({
+        status: HttpStatus.CREATED,
+        message: MensagensUsuario.USUARIO_CRIADO,
+        data: usuarioCriado
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Put()
+  async editarUsuario(
+    @Body() editarUsuarioDto: EditarUsuarioDto,
+    @Res() response: Response,
+  ): Promise<Response> {
+    try {
+      const usuarioEditado: Usuario =
+        await this.usuarioService.editarUsuario(editarUsuarioDto);
+      return response.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: MensagensUsuario.USUARIO_EDITADO,
+        data: usuarioEditado
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Get('/:id')
+  async getUserById(
+    @Param() id: IdDto,
+    @Res() response: Response,
+  ): Promise<Response> {
+    const connectionDetails = {
+      name: this.dataSource.name,
+      type: this.dataSource.options.type,
+      database: this.dataSource.options.database,
+      isConnected: this.dataSource.isInitialized,
+    };
+
+    try {
+      const usuario: Partial<Usuario> =
+        await this.usuarioService.getUsuarioById(id.id);
+      // return response.status(HttpStatus.OK).send({ ...usuario, connectionDetails });
+      return response.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        data: usuario
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Delete('/:id')
+  async deletarUsuario(
+    @Param() id: IdDto,
+    @Res() response: Response,
+  ): Promise<Response> {
+    try {
+      const nomeUsuarioExcluido: string =
+        await this.usuarioService.deleteUsuario(id.id);
+      return response
+        .status(HttpStatus.OK)
+        .send({
+          status: HttpStatus.OK,
+          message: MensagensUsuario.USUARIO_EXCLUIDO(nomeUsuarioExcluido)
+        });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Post('auth')
+  async login(
+      @Body() loginDto: LoginDTO,
+      @Res() response: Response
+  ): Promise<Response> {
+    try {
+      const accessToken: AccessToken = await this.usuarioService.login(loginDto);
+      return response.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: MensagensUsuario.USUARIO_AUTENTICADO,
+        data: accessToken
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Get()
+  async buscarUsuariosPaginado(
+      @Query() paginacao: PaginacaoDto,
+      @Res() response: Response,
+  ): Promise<Response> {
+    try {
+      const respostaPaginada: RespostaPaginada<Usuario> = await this.usuarioService.buscarUsuariosPaginado(paginacao);
+      return response.status(HttpStatus.OK).send({
+        status: HttpStatus.OK,
+        message: MensagensInsumos.INSUMO_ENCONTRADOS,
+        data: respostaPaginada
+      });
+    } catch (e) {
+      throw (e);
+    }
+  }
+}
---
+++ src/service/usuario/usuario.service.ts
@@ -0,0 +1,150 @@
+import {
+  ConflictException,
+  HttpStatus,
+  Injectable,
+  NotFoundException, UnauthorizedException,
+} from '@nestjs/common';
+import { UsuarioRepository } from '../../repository/usuario/usuario.repository';
+import { CriarUsuarioDto } from '../../model/usuario/dto/criar-usuario.dto';
+import { Usuario } from '../../model/usuario/usuario.entity';
+import * as bcrypt from 'bcrypt';
+import { MensagensUsuario } from '../../model/usuario/utils/mensagens-usuario';
+import { EditarUsuarioDto } from '../../model/usuario/dto/editar-usuario.dto';
+import { NomeDoUsuario } from '../../model/usuario/nome-usuario.type';
+import { Count } from '../../shared/interfaces/count.interface';
+import {LoginDTO} from "../../model/usuario/dto/login.dto";
+import {AccessToken} from "../../model/auth/interfaces/access-token.interface";
+import {AuthService} from "../auth/auth.service";
+import {PaginacaoDto} from "../../shared/dto/paginacao.dto";
+import {RespostaPaginada} from "../../shared/interfaces/resposta-paginada.interface";
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+      private readonly usuarioRepository: UsuarioRepository,
+      private readonly authService: AuthService
+  ) {}
+
+  async criarUsuario(criarUsuarioDto: CriarUsuarioDto): Promise<Usuario> {
+    const usuario: Usuario =
+      await this.instanciarUsuarioByDtoDeCriacao(criarUsuarioDto);
+    const deveExistir: boolean = false;
+    await this.validarExistenciaDoUsuario(new Usuario({ username: usuario.username }), deveExistir);
+    const usuarioCriado: Usuario = await this.usuarioRepository.saveUsuario(usuario);
+    return this.instanciarUsuarioRetirandoDadosSensiveis(usuarioCriado);
+  }
+
+  async validarExistenciaDoUsuario(
+    usuario: Partial<Usuario>,
+    deveExistir: boolean,
+  ): Promise<void> {
+    const usuarioExiste: boolean =
+      await this.usuarioRepository.verificarExistenciaDoUsuario(usuario);
+    if (usuarioExiste && !deveExistir) {
+      throw new ConflictException({
+        status: HttpStatus.CONFLICT,
+        message: MensagensUsuario.USUARIO_JA_EXISTE,
+      });
+    }
+    if (!usuarioExiste && deveExistir) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        message: MensagensUsuario.USUARIO_NAO_EXISTE,
+      });
+    }
+  }
+
+  async editarUsuario(editarUsuarioDto: EditarUsuarioDto): Promise<Usuario> {
+    const usuario: Usuario =
+      await this.instanciarUsuarioByDtoDeCriacao(editarUsuarioDto);
+    usuario.id = editarUsuarioDto.id;
+    const deveExistir: boolean = true;
+    await this.validarExistenciaDoUsuario(
+      new Usuario({ id: usuario.id }),
+      deveExistir,
+    );
+    const usuarioEditado: Usuario = await this.usuarioRepository.saveUsuario(usuario);
+    return this.instanciarUsuarioRetirandoDadosSensiveis(usuarioEditado);
+  }
+
+  async instanciarUsuarioByDtoDeCriacao(
+    criarUsuarioDto: CriarUsuarioDto,
+  ): Promise<Usuario> {
+    const { name, senha, email, nivelPermissao } = criarUsuarioDto;
+    const salt: string = await bcrypt.genSalt();
+    return new Usuario({
+      username: name,
+      salt: salt,
+      senha: await bcrypt.hash(senha, salt),
+      email: email,
+      nivelPermissao: nivelPermissao,
+      isExcluido: false,
+    });
+  }
+
+  async deleteUsuario(id: number): Promise<NomeDoUsuario> {
+    const usuario: Usuario | null =
+      await this.usuarioRepository.findOneById(id);
+    if (!usuario || usuario.isExcluido) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        message: MensagensUsuario.USUARIO_NAO_EXISTE,
+      });
+    }
+    await this.usuarioRepository.performarExclusaoLogicaDeUsuario(usuario.id);
+    return usuario.username;
+  }
+
+  async getUsuarioById(usuarioId: number): Promise<Partial<Usuario>> {
+    const usuario: Usuario | null =
+      await this.usuarioRepository.getUserById(usuarioId);
+    if (!usuario || usuario.isExcluido) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        message: MensagensUsuario.USUARIO_NAO_EXISTE,
+      });
+    }
+    const { id, username, email, nivelPermissao, isExcluido } = usuario;
+    return new Usuario({
+      id: id,
+      username: username,
+      email: email,
+      nivelPermissao: nivelPermissao,
+      isExcluido: isExcluido
+    });
+  }
+
+  instanciarUsuarioRetirandoDadosSensiveis(usuario: Usuario): Usuario {
+    const { id, username, email, nivelPermissao, isExcluido } = usuario;
+    return new Usuario({
+      id: id,
+      username: username,
+      email: email,
+      nivelPermissao: nivelPermissao,
+      isExcluido: isExcluido
+    });
+  }
+
+  async login(loginDto: LoginDTO): Promise<AccessToken> {
+    const { email, senha } = loginDto;
+    const usuario: Usuario | null = await this.usuarioRepository.buscarUsuario({
+      email: email
+    });
+
+    if (!usuario) {
+      throw new UnauthorizedException({
+        status: HttpStatus.UNAUTHORIZED,
+        message: MensagensUsuario.EMAIL_OU_SENHA_INCORRETOS
+      });
+    }
+    return await this.authService.login(usuario, senha);
+  }
+
+  async buscarUsuariosPaginado(paginacao: PaginacaoDto): Promise<RespostaPaginada<Usuario>> {
+    const { pagina, limite } = paginacao;
+    return await this.usuarioRepository.buscarUsuariosPaginado(
+        pagina,
+        limite
+    );
+  }
+}
